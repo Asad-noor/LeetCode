3f06a1363e54d90dcf1663624fd5396562bf7dca
Md. Asaduzzaman Noor <md.noor.asad@gmail.com>
2019-12-22 18:14:59 +0600

    add two sum ll problem solution.

---
+++ two_sum_ll.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        List<Integer> list = new ArrayList<>();
+        int k = 0;
+        int i;
+        for(i = 0; i < nums.length; i++) {
+            int value = target - nums[i];
+            if(list.contains(value)) {
+                k = list.indexOf(value);
+                break;
+            } else {
+                list.add(nums[i]);
+            }
+        }
+        k++;
+        i++;
+        if(k > i) {
+            return new int[] {i , k};
+        } else {
+            return new int[] {k , i};  
+        }
+    }
+}
