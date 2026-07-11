efdeba2345b29670da089d2aa1f4b99c6f2d163d
Md. Asaduzzaman Noor <md.noor.asad@gmail.com>
2026-07-11 13:33:22 +0600

    Create solution valid_parentheses.kt

---
+++ valid_parentheses.kt
@@ -0,0 +1,31 @@
+https://leetcode.com/problems/valid-parentheses/description/
+
+
+class Solution {
+    fun isValid(s: String): Boolean {
+        val stack = Stack<Char>()
+
+        for(ch in s) {
+            if(ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch)
+            } else {
+
+                if (stack.isEmpty()) {
+                    return false
+                }   
+
+                val top = stack.pop()
+
+                if(
+                    (ch == ')' && top != '(' ) ||
+                    (ch == '}' && top != '{' ) ||
+                    (ch == ']' && top != '[' )
+                ) {
+                    return false
+                }
+            }
+        }
+
+        return stack.isEmpty()
+    }
+}
